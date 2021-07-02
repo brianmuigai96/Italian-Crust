b1a34f6d9428803fa232ede0e84c8547fd2abc16
brianmuigai96 <brian.muigai@student.moringaschool.com>
2021-07-02 10:35:39 +0300

    adding content to the script file

---
+++ js/script.js
@@ -0,0 +1,63 @@
+function getSizeCost() {
+    var selectedSize = document.getElementById("size").value;
+    return parseInt(selectedSize);
+}
+
+function getCrustCost() {
+    var selectedCrust = document.getElementById("crust").value;
+    return parseInt(selectedCrust);
+}
+function getNumber() {
+    var selectedNumber = document.getElementById("numberofpizza").value;
+    return parseInt(selectedNumber);
+}
+function mushrooms() {
+    var mushroom = 0;
+    var addMushroom = document.getElementById("toppingone");
+    if (addMushroom.checked === true) {
+        mushroom = 200;
+    }
+    return parseInt(mushroom);
+}
+
+function sausages() {
+    var sausage = 0;
+    var addsausage = document.getElementById("toppingone");
+    if (addsausage.checked === true) {
+        sausage = 150;
+    }
+    return parseInt(sausage);
+}
+function onions() {
+    var onion = 0;
+    var addonions = document.getElementById("toppingone");
+    if (addMushroom.checked === true) {
+        onion = 50;
+    }
+    return parseInt(onion);
+}
+function tikkaSaucee() {
+    var tikkaSauce = 0;
+    var addtikkaSauce = document.getElementById("toppingone");
+    if (addtikkaSauce.checked === true) {
+        tikkaSauce = 300;
+    }
+    return parseInt(tikkaSauce);
+}
+function mozarellaCheese() {
+    var cheese = 0;
+    var addmozarellaCheese = document.getElementById("toppingone");
+    if (addmozarellaCheese.checked === true) {
+        cheese = 400;
+    }
+    return parseInt(cheese);
+}
+
+function calctotalPrice(e) {
+    event.preventDefault();
+    var totalPrice = (getSizeCost() + getCrustCost() + mushrooms()) * (getNumber());
+
+        console.log(totalPrice);
+    alert("Your order of " + getNumber() + " pizzas has been processed.Your total amount payable is " + totalPrice +"." +"thanks for choosing for Italian Crust" )
+
+}
